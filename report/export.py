c22fe31c5c7327f5ecec46dec30c8960d218dd64
Marti Kecskemeti <martikecskemeti@gmail.com>
2016-11-12 19:08:33 +0100

    hw

--- report/export.py
+++ report/export.py
@@ -1,24 +1,32 @@
 import reports
 # Export functions
 
+functions = [reports.count_games("game_stat.txt"),
+             reports.decide("game_stat.txt",2000),
+             reports.get_latest("game_stat.txt"),
+             reports.count_by_genre("game_stat.txt","First-person shooter"),
+             reports.get_line_number_by_title("game_stat.txt","Counter-Strike"),
+             reports.sort_abc("game_stat.txt"),
+             reports.get_genres("game_stat.txt"),
+             reports.when_was_top_sold_fps("game_stat.txt")]
+
+
 def export(source_file_name,output_file_name):
     reports_export = open(output_file_name, "w+")
-    reports_export.writelines("How many games are in the file?:" + "\n")
-    reports_export.writelines(str(reports.count_games(source_file_name)) + "\n")
-    reports_export.writelines("Is there a game from a given year?:" + "\n")
-    reports_export.writelines(str(reports.decide(source_file_name,2000))+ "\n")
-    reports_export.writelines("Which was the latest game?:" + "\n")
-    reports_export.writelines(str(reports.get_latest(source_file_name))+ "\n")
-    reports_export.writelines("How many games do we have by genre?:" + "\n")
-    reports_export.writelines(str(reports.count_by_genre(source_file_name,"First-person shooter"))+ "\n")
-    reports_export.writelines("What is the line number of the given game (by title)?:" + "\n")
-    reports_export.writelines(str(reports.get_line_number_by_title(source_file_name,"Counter-Strike"))+ "\n")
-    reports_export.writelines("What is the alphabetical ordered list of the titles?:" + "\n")
-    reports_export.writelines(str(reports.sort_abc(source_file_name))+ "\n")
-    reports_export.writelines("What are the genres?:" + "\n")
-    reports_export.writelines(str(reports.get_genres(source_file_name))+ "\n")
-    reports_export.writelines("What is the release date of the top sold 'First-person shooter' game?:" + "\n")
-    reports_export.writelines(str(reports.when_was_top_sold_fps(source_file_name)))
+    for item in functions:
+        reports_export.writelines(item.__doc__ + "\n" + str(item) + "\n")
     reports_export.close()
+
+"""def export(source_file_name,output_file_name):
+    reports_export = open(output_file_name, "w+")
+    reports_export.writelines(reports.count_games.__doc__ + "\n" + str(reports.count_games(source_file_name)) + "\n")
+    reports_export.writelines(str(reports.decide(source_file_name,2000))+ "\n")
+    reports_export.writelines(str(reports.get_latest(source_file_name))+ "\n")
+    reports_export.writelines(str(reports.count_by_genre(source_file_name,"First-person shooter"))+ "\n")
+    reports_export.writelines(str(reports.get_line_number_by_title(source_file_name,"Counter-Strike"))+ "\n")
+    reports_export.writelines(str(reports.sort_abc(source_file_name))+ "\n")
+    reports_export.writelines(str(reports.get_genres(source_file_name))+ "\n")
+    reports_export.writelines(str(reports.when_was_top_sold_fps(source_file_name)))
+    reports_export.close()"""
 
 export("game_stat.txt","reports.txt")
--- report/reports.py
+++ report/reports.py
@@ -1,12 +1,13 @@
 
 # Report functions
 
-
 def count_games(file_name):
+    '''How many games are in the file?'''
     with open(file_name) as game_inv:
         return sum(1 for line in game_inv)
 
 def decide(file_name, year):
+    '''Is there a game from a given year?'''
     with open(file_name) as game_inv:
         for line in game_inv:
             if line.split("\t")[2] == str(year):
@@ -14,7 +15,8 @@ def decide(file_name, year):
         return False
 
 def get_latest(file_name):
-    game = open("game_stat.txt", "r")
+    '''Which was the latest game?'''
+    game = open(file_name, "r")
     game_inv = game.readlines()
     game.close()
     maximum = max(int(line.split("\t")[2]) for line in game_inv)
@@ -23,48 +25,51 @@ def get_latest(file_name):
             return line.split("\t")[0]
 
 def count_by_genre(file_name, genre):
+    '''How many games do we have by genre?'''
     with open(file_name) as game_inv:
-        return sum(1 for x in [line.split("\t") for line in game_inv] if x[3] == genre)
+        return sum(1 for game_genre in [line.split("\t")[3] for line in game_inv] if game_genre == genre)
 
 def get_line_number_by_title(file_name, title):
+    '''What is the line number of the given game (by title)?'''
     with open(file_name) as game_inv:
         num = 0
         for line in game_inv:
             num += 1
             if line.split("\t")[0] == str(title):
                 return num
+        return "There is no game with this title."
 
 def sort_abc(file_name):
-    game = open("game_stat.txt", "r")
+    '''What is the alphabetical ordered list of the titles?'''
+    game = open(file_name, "r")
     game_inv = game.readlines()
     game.close()
     titles = [x[0] for x in [line.split("\t") for line in game_inv]]
     sorted = []
     for i in range(len(titles)):
         for n in range(len(titles)):
-            if titles[i] >= titles[n]:
-                titles[i] = titles[i]
-            else:
+            if titles[i] < titles[n]:
                 titles[i], titles[n] = titles[n], titles[i]
     return titles
 
 def get_genres(file_name):
+    '''What are the genres?'''
     with open(file_name) as game_inv:
         genres_list = []
         genres = [genres_list.append(x[3]) for x in [line.split("\t") for line in game_inv] if x[3] not in genres_list]
         return sorted(genres_list, key = str.lower)
 
 def when_was_top_sold_fps(file_name):
-    game = open("game_stat.txt", "r")
+    '''What is the release date of the top sold "First-person shooter" game?'''
+    game = open(file_name, "r")
     game_inv = game.readlines()
     game.close()
+    genre = "First-person shooter"
     try:
-        maximum = max(float(line.split("\t")[1]) for line in game_inv if line.split("\t")[3] == "First-person shooter")
+        maximum = max(float(line.split("\t")[1]) for line in game_inv if line.split("\t")[3] == genre)
     except ValueError:
-        return ("There is no First-person shooter")
+        return ("There is no %s" % genre)
     for line in game_inv:
-        if line.split("\t")[3] == "First-person shooter":
+        if line.split("\t")[3] == genre:
             if float(line.split("\t")[1]) == maximum:
                 return int(line.split("\t")[2])
-
-
